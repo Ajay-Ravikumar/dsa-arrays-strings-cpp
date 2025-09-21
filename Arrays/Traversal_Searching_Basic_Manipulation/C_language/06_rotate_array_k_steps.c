f264db2158c5e30324bf49b9e056e4d002490305
Ajay-Ravikumar <ajay.ravikumar@yahoo.com>
2025-09-21 10:17:37 +0200

    Create 06_rotate_array_k_steps.c

---
+++ Arrays/Traversal_Searching_Basic_Manipulation/C_language/06_rotate_array_k_steps.c
@@ -0,0 +1,28 @@
+/* Rotate an Array by k Steps (Right Rotation) */
+
+#include <stdio.h>
+
+void reverse(int arr[], int l, int r) {
+    while (l < r) {
+        int tmp = arr[l];
+        arr[l] = arr[r];
+        arr[r] = tmp;
+        l++; r--;
+    }
+}
+
+void rotate(int arr[], int n, int k) {
+    k = k % n;
+    reverse(arr, 0, n-1);
+    reverse(arr, 0, k-1);
+    reverse(arr, k, n-1);
+}
+
+int main() {
+    int arr[] = {1,2,3,4,5,6,7};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    int k = 3;
+    rotate(arr, n, k);
+    for (int i=0; i<n; i++) printf("%d ", arr[i]);
+    return 0;
+}
