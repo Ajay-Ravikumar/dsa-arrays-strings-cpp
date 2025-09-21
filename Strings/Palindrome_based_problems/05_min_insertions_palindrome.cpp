b9d3c0e0f35918b6f8c50658e8721f619190565f
Ajay-Ravikumar <ajay.ravikumar@yahoo.com>
2025-09-21 11:45:20 +0200

    Create 05_min_insertions_palindrome.cpp

---
+++ Strings/Palindrome_based_problems/05_min_insertions_palindrome.cpp
@@ -0,0 +1,26 @@
+/* Minimum Insertions to Make String Palindrome */
+
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int minInsertions(string s) {
+    int n=s.size();
+    vector<vector<int>> dp(n,vector<int>(n,0));
+    for (int i=0;i<n;i++) dp[i][i]=1;
+    for (int len=2;len<=n;len++) {
+        for (int i=0;i<=n-len;i++) {
+            int j=i+len-1;
+            if (s[i]==s[j] && len==2) dp[i][j]=2;
+            else if (s[i]==s[j]) dp[i][j]=dp[i+1][j-1]+2;
+            else dp[i][j]=max(dp[i+1][j],dp[i][j-1]);
+        }
+    }
+    return n - dp[0][n-1];
+}
+
+int main() {
+    string s="mbadm";
+    cout << "Min Insertions = " << minInsertions(s) << endl;
+}
