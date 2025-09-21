f3f703a37cbebd2f913a56e7b1d6ba11a5d122a7
Ajay-Ravikumar <ajay.ravikumar@yahoo.com>
2025-09-21 11:43:48 +0200

    Create 03_longest_palindrome_subsequence.cpp

---
+++ Strings/Palindrome_based_problems/03_longest_palindrome_subsequence.cpp
@@ -0,0 +1,26 @@
+/* Longest Palindromic Subsequence */
+
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int longestPalSubseq(string s) {
+    int n=s.size();
+    vector<vector<int>> dp(n,vector<int>(n,0));
+    for (int i=0;i<n;i++) dp[i][i]=1;
+    for (int len=2;len<=n;len++) {
+        for (int i=0;i<=n-len;i++) {
+            int j=i+len-1;
+            if (s[i]==s[j] && len==2) dp[i][j]=2;
+            else if (s[i]==s[j]) dp[i][j]=dp[i+1][j-1]+2;
+            else dp[i][j]=max(dp[i+1][j],dp[i][j-1]);
+        }
+    }
+    return dp[0][n-1];
+}
+
+int main() {
+    string s="bbbab";
+    cout << "Longest Palindromic Subsequence = " << longestPalSubseq(s) << endl;
+}
